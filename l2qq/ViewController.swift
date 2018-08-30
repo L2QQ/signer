9dbd9e7d4f2c509400514e016bedaeb079664bad
devmanio <ivan.kocheshev@gmail.com>
2018-08-30 19:49:10 +0300

    Initial Commit

---
+++ l2qq/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  l2qq
+//
+//  Created by im on 30/08/2018.
+//  Copyright © 2018 l2. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
